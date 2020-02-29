dc0aca9904924e29862083a6b31322d6db515b05
Bibek Shretha <bibekshrestha@Bibeks-Work-Tool.local>
2020-02-28 20:34:24 -0500

    separate heroes into their attributes

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -1,23 +1,42 @@
 <template>
   <div class='container'>
-    <h2>Heroes</h2>
-      <div class="row">
-        <div v-for='hero in heroes' :key='hero.id' class='col-3'>
-          <img :src='`https://api.opendota.com${hero.img}`' alt=''>
-          {{ hero.localized_name }}
+    <h2 class="display-4">Heroes</h2>
+    <h3>Strength</h3>
+      <div class="row mb-5">
+        <div v-for='hero in strHeroes' :key='hero.id' class='col-4 col-md-3 p-4'>
+          <span><img :src='`https://api.opendota.com${hero.img}`' alt='' class="mb-2">
+          {{ hero.localized_name }}</span>
         </div>
       </div>
-    
+    <h3>Agility</h3>
+      <div class="row">
+        <div v-for='hero in agiHeroes' :key='hero.id' class='col-4 col-md-3 p-4'>
+          <span><img :src='`https://api.opendota.com${hero.img}`' alt='' class="mb-2">
+          {{ hero.localized_name }}</span>
+        </div>
+      </div>
+    <h3>Intelligence</h3>
+      <div class="row">
+        <div v-for='hero in intHeroes' :key='hero.id' class='col-4 col-md-3 p-4'>
+          <span><img :src='`https://api.opendota.com${hero.img}`' alt='' class="mb-2">
+          {{ hero.localized_name }}</span>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash/core'
+
 export default {
   name: 'Heroes',
   components: {},
   data () {
     return {
-      heroes: []
+      heroes: [],
+      strHeroes: [],
+      agiHeroes: [],
+      intHeroes: []
     }
   },
   mounted () {
@@ -25,7 +44,14 @@ export default {
       .then(res => {
         return res.json()
       })
-      .then(data => { this.heroes = data })
+      .then(data => {
+        console.log(data[0])
+        this.heroes = data
+        _.forEach(data, hero => {
+          let heroAttr = hero.primary_attr
+          heroAttr === 'str' ? this.strHeroes.push(hero) : heroAttr === 'agi' ? this.agiHeroes.push(hero) : this.intHeroes.push(hero)
+        })
+      })
   }
 }
 </script>
